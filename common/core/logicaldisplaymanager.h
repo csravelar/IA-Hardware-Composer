4a297aec02f8eb3d6a607acb510dfcd8f4667625
Richard Avelar <richard.avelar@intel.com>
2018-07-31 10:00:53 -0700

    logicaldisplaymanager.h

--- common/core/logicaldisplaymanager.h
+++ common/core/logicaldisplaymanager.h
@@ -29,21 +29,55 @@ class LogicalDisplayManager;
 
 class LogicalDisplayManager {
  public:
+  /**
+  * Constructor that sets the physical display for the class.
+  *
+  * @param physical_display a NativeDisplay object representing a physical
+  *                         display
+  */
   LogicalDisplayManager(NativeDisplay* physical_display);
   ~LogicalDisplayManager();
 
+  /**
+  * Initialize new logical displays up to the passed in total count.
+  *
+  * @param total an unsigned 32 bit integer to dictate the amount of logical
+  *              displays to initialize
+  */
   void InitializeLogicalDisplays(uint32_t total);
+  /**
+  * Switch the physical display's power mode to on or off based on previous state.
+  */
   void UpdatePowerMode();
+  /**
+  * Enable or disable the VSyncControl based on the physical display's EnableVSync.
+  */
   void UpdateVSyncControl();
+  /**
+  * Register the hot plug and notifications if the hot plug is not already registered.
+  */
   void RegisterHotPlugNotification();
 
   bool Present(std::vector<HwcLayer*>& source_layers, int32_t* retire_fence,
                PixelUploaderCallback* call_back, bool handle_constraints);
 
+  /**
+  * Run all display's VSyncUpdate and pass timestamp to each call.
+  *
+  * @param timestamp a 64 bit integer
+  */
   void VSyncCallback(int64_t timestamp);
 
+  /**
+  * Run all display's RefreshUpdate
+  */
   void RefreshCallback();
 
+  /**
+  * Run all display's HotPlugUpdate and pass the parameter connected to each.
+  *
+  * @param connected a boolean value
+  */
   void HotPlugCallback(bool connected);
 
   NativeDisplay* GetPhysicalDisplay() const {
